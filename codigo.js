75d05fb2d47e1e57be17bd8c861ae2eeb42ade2f
EstebanDBH1 <120125487+EstebanDBH1@users.noreply.github.com>
2023-01-25 17:32:51 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,8 @@
+let openMenu = document.querySelector(".fa-bars");
+let menu = document.querySelector(".lista");
+
+openMenu.addEventListener("click", (e) => {
+  menu.classList.toggle("disable");
+});
+
+
